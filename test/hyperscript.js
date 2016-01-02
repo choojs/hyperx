166ad6e4957d197333a3f3de1507e3d792d24de5
James Halliday <mail@substack.net>
2016-01-02 07:09:59 -0500

    failing hyperscript test

---
+++ test/hyperscript.js
@@ -0,0 +1,26 @@
+var test = require('tape')
+var h = require('hyperscript')
+var hyperx = require('../')
+var hx = hyperx(h)
+
+var expected = `<div>
+    <h1 y="ab3cd">hello world!</h1>
+    <i>cool</i>
+    wow
+    <b>1</b><b>2</b><b>3</b>
+  </div>`
+
+test('hyperscript', function (t) {
+  var title = 'world'
+  var wow = [1,2,3]
+  var tree = hx`<div>
+    <h1 y="ab${1+2}cd">hello ${title}!</h1>
+    ${hx`<i>cool</i>`}
+    wow
+    ${wow.map(function (w) {
+      return hx`<b>${w}</b>\n`
+    })}
+  </div>`
+  t.equal(tree.outerHTML, expected)
+  t.end()
+})
