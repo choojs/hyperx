b5a2cddff9c68fe55734ac371455f6e95d3c8d3f
James Halliday <mail@substack.net>
2016-01-02 03:37:25 -0500

    scope issue

--- index.js
+++ index.js
@@ -5,9 +5,8 @@ var ATTR_VALUE_SQ = 9, ATTR_VALUE_DQ = 10
 var ATTR_VALUE_SQ_E = 11, ATTR_VALUE_DQ_E = 12
 
 module.exports = function (h) {
-  var state = TEXT, reg = ''
-  var tree = {}
   return function (strings) {
+    var state = TEXT, reg = ''
     var arglen = arguments.length
     var parts = []
     for (var i = 0; i < strings.length; i++) {
@@ -18,80 +17,82 @@ module.exports = function (h) {
       } else parts.push(parse(strings[i]))
     }
     return parts
-  }
-  function parse (str) {
-    var res = []
-    for (var i = 0; i < str.length; i++) {
-      var c = str.charAt(i)
-      if (state === TEXT && c === '<') {
-        if (reg.length) res.push([TEXT, reg])
-        reg = ''
-        state = OPEN
-      } else if (c === '>' && !quot(state)) {
-        if (state === OPEN) {
-          res.push([OPEN,reg])
-        } else if (state === ATTR_KEY) {
+
+    function parse (str) {
+      var res = []
+      for (var i = 0; i < str.length; i++) {
+        var c = str.charAt(i)
+        if (state === TEXT && c === '<') {
+          if (reg.length) res.push([TEXT, reg])
+          reg = ''
+          state = OPEN
+        } else if (c === '>' && !quot(state)) {
+          if (state === OPEN) {
+            res.push([OPEN,reg])
+          } else if (state === ATTR_KEY) {
+            res.push([ATTR_KEY,reg])
+          } else if (state === ATTR_VALUE && reg.length) {
+            res.push([ATTR_VALUE,reg])
+          }
+          res.push([CLOSE])
+          reg = ''
+          state = TEXT
+        } else if (state === TEXT) {
+          reg += c
+        } else if (state === OPEN && /\s/.test(c)) {
+          res.push([OPEN, reg])
+          reg = ''
+          state = ATTR
+        } else if (state === OPEN) {
+          reg += c
+        } else if (state === ATTR && /[\w-]/.test(c)) {
+          state = ATTR_KEY
+          reg = c
+        } else if (state === ATTR_KEY && /\s/.test(c)) {
           res.push([ATTR_KEY,reg])
-        } else if (state === ATTR_VALUE && reg.length) {
+          reg = ''
+          state = ATTR_KEY_W
+        } else if (state === ATTR_KEY && c === '=') {
+          res.push([ATTR_KEY,reg])
+          reg = ''
+          state = ATTR_VALUE_W
+        } else if (state === ATTR_KEY_W && c === '=') {
+          state = ATTR_VALUE_W
+        } else if (state === ATTR_KEY_W && !/\s/.test(c)) {
+          state = ATTR
+          i--
+        } else if (state === ATTR_VALUE_W && c === '"') {
+          state = ATTR_VALUE_DQ
+        } else if (state === ATTR_VALUE_W && c === "'") {
+          state = ATTR_VALUE_SQ
+        } else if (state === ATTR_VALUE_DQ && c === '"') {
+          res.push([ATTR_VALUE_DQ,reg])
+          reg = ''
+          state = ATTR
+        } else if (state === ATTR_VALUE_SQ && c === '"') {
+          res.push([ATTR_VALUE_SQ,reg])
+          reg = ''
+          state = ATTR
+        } else if (state === ATTR_VALUE_DQ && c === '\\') {
+          state = ATTR_VALUE_DQ_E
+          reg += c
+        } else if (state === ATTR_VALUE_SQ && c === '\\') {
+          state = ATTR_VALUE_SQ_E
+          reg += c
+        } else if (state === ATTR_VALUE_W && !/\s/.test(c)) {
+          state = ATTR_VALUE
+          i--
+        } else if (state === ATTR_VALUE && /\s/.test(c)) {
           res.push([ATTR_VALUE,reg])
+          reg = ''
+          state = ATTR
+        } else if (state === ATTR_VALUE || state === ATTR_VALUE_SQ
+        || state === ATTR_VALUE_DQ) {
+          reg += c
         }
-        res.push([CLOSE])
-        reg = ''
-        state = TEXT
-      } else if (state === TEXT) {
-        reg += c
-      } else if (state === OPEN && /\s/.test(c)) {
-        res.push([OPEN, reg])
-        reg = ''
-        state = ATTR
-      } else if (state === OPEN) {
-        reg += c
-      } else if (state === ATTR && /[\w-]/.test(c)) {
-        state = ATTR_KEY
-        reg = c
-      } else if (state === ATTR_KEY && /\s/.test(c)) {
-        res.push([ATTR_KEY,reg])
-        reg = ''
-        state = ATTR_KEY_W
-      } else if (state === ATTR_KEY && c === '=') {
-        res.push([ATTR_KEY,reg])
-        reg = ''
-        state = ATTR_VALUE_W
-      } else if (state === ATTR_KEY_W && c === '=') {
-        state = ATTR_VALUE_W
-      } else if (state === ATTR_KEY_W && !/\s/.test(c)) {
-        state = ATTR
-        i--
-      } else if (state === ATTR_VALUE_W && c === '"') {
-        state = ATTR_VALUE_DQ
-      } else if (state === ATTR_VALUE_W && c === "'") {
-        state = ATTR_VALUE_SQ
-      } else if (state === ATTR_VALUE_DQ && c === '"') {
-        res.push([ATTR_VALUE_DQ,reg])
-        reg = ''
-        state = ATTR
-      } else if (state === ATTR_VALUE_SQ && c === '"') {
-        res.push([ATTR_VALUE_SQ,reg])
-        reg = ''
-        state = ATTR
-      } else if (state === ATTR_VALUE_DQ && c === '\\') {
-        state = ATTR_VALUE_DQ_E
-        reg += c
-      } else if (state === ATTR_VALUE_SQ && c === '\\') {
-        state = ATTR_VALUE_SQ_E
-        reg += c
-      } else if (state === ATTR_VALUE_W && !/\s/.test(c)) {
-        state = ATTR_VALUE
-        i--
-      } else if (state === ATTR_VALUE && /\s/.test(c)) {
-        res.push([ATTR_VALUE,reg])
-        state = ATTR
-      } else if (state === ATTR_VALUE || state === ATTR_VALUE_SQ
-      || state === ATTR_VALUE_DQ) {
-        reg += c
       }
+      return res
     }
-    return res
   }
 }
 
