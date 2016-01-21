3ba17cedeabcc7f9f3bfbcdf3205ff5d17b32e9f
Robert Sköld <robert@publicclass.se>
2016-01-21 09:03:02 +0100

    Added test for single quote attributes

--- test/esc.js
+++ test/esc.js
@@ -3,12 +3,22 @@ var vdom = require('virtual-dom')
 var hyperx = require('../')
 var hx = hyperx(vdom.h)
 
-test('escape', function (t) {
+test('escape double quotes', function (t) {
   var value = '">'
   var tree = hx`<input type="text" value="${value}"></h1>`
   t.equal(
     vdom.create(tree).toString(),
   `<input type="text" value="&quot;&gt;" />`
+  )
+  t.end()
+})
+
+test('escape single quotes', function (t) {
+  var value = "'>"
+  var tree = hx`<input type='text' value='${value}'></h1>`
+  t.equal(
+    vdom.create(tree).toString(),
+  `<input type="text" value="'&gt;" />`
   )
   t.end()
 })
