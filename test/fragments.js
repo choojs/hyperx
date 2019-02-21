1f85d7bc3bde45fe55528a842083eb8c0bdbeaea
Renée Kooi <renee@kooi.me>
2019-02-21 14:34:47 +0100

    Revert "Treat single top-level array template part as a fragment (#74)"
    This reverts commit a535fd5b119a7ca1750ad53e860167409fc4def5.

--- test/fragments.js
+++ test/fragments.js
@@ -4,26 +4,14 @@ var hyperx = require('../')
 var hx = hyperx(vdom.h, {createFragment: createFragment})
 
 function createFragment (nodes) {
-  return { frag: nodes }
+  return nodes
 }
 
-test('multiple root, fragments as array', function (t) {
+test('mutliple root, fragments as array', function (t) {
   var list = hx`<li>1</li>  <li>2<div>_</div></li>`
-  t.ok(Array.isArray(list.frag))
-  t.equal(list.frag.length, 3, '3 elements')
-  t.equal(vdom.create(list.frag[0]).toString(), '<li>1</li>')
-  t.equal(list.frag[1], '  ')
-  t.equal(vdom.create(list.frag[2]).toString(), '<li>2<div>_</div></li>')
-  t.end()
-})
-
-test('multiple root embeds, fragments as array', function (t) {
-  var list = hx`
-    ${[1,2]}
-  `
-  t.ok(Array.isArray(list.frag))
-  t.deepEqual(list, {
-    frag: [1, 2]
-  })
+  t.equal(list.length, 3, '3 elements')
+  t.equal(vdom.create(list[0]).toString(), '<li>1</li>')
+  t.equal(list[1], '  ')
+  t.equal(vdom.create(list[2]).toString(), '<li>2<div>_</div></li>')
   t.end()
 })
